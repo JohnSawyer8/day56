ca1f83efde644d42928ef846d694cf78786d3ce2
johnsawyer <johnsawyer@seattleacademy.orggit config --global user.name johnsawyergit config --global user.email johnsawyer@seattleacademy.org>
2019-03-08 09:50:47 -0800

    added socket.io

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+
+
 
 app.use(express.static('dist'));
 
@@ -12,7 +17,7 @@ app.use('/dist', express.static('dist'));
 
 
 //identify unused port between 1520 and 1540
-var port = 1541;
+var port = 1538;
 http.listen(port, function(){
   console.log('listening on https://seattleacademy.software/'+port);
 });
